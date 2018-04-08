c7a028b3e29dd79db792a698f56a7e573fa1fca8
Andrés Hernández <andreshj87@gmail.com>
2018-04-08 16:35:45 +0200

    Add logic to draw items dynamically

--- app/src/main/java/com/andreshj87/radialpager/demo/MainActivity.kt
+++ app/src/main/java/com/andreshj87/radialpager/demo/MainActivity.kt
@@ -11,16 +11,51 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    drawItems()
+  }
+
+  fun drawItems() {
     val radialPager = findViewById<RadialPager<Person>>(R.id.radialpager)
 
     val person = Person("Putin",
         "https://static.timesofisrael.com/www/uploads/2018/03/000_12537P-e1520673369804-640x400.jpg")
 
-    var radialPagerItem = RadialPagerItem.Builder<Person>()
+    val radialPagerItem = RadialPagerItem.Builder<Person>()
         .data(person)
         .imageResource(R.drawable.putin)
         .build()
 
     radialPager.setCenterItem(radialPagerItem)
+
+    val items: ArrayList<RadialPagerItem<Person>> = ArrayList()
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+    items.add(radialPagerItem)
+
+    radialPager.setItems(items)
   }
 }
--- library/src/main/java/com/andreshj87/radialpager/RadialPager.kt
+++ library/src/main/java/com/andreshj87/radialpager/RadialPager.kt
@@ -7,11 +7,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
 
 class RadialPager<T> : ConstraintLayout {
 
+  private val MAX_LAYERS = 3
+  private val MAX_ITEMS_PER_LEVEL = 6
+
+  private val itemSizes: ArrayList<Float> = ArrayList(MAX_LAYERS)
+  private val itemRadius: ArrayList<Float> = ArrayList(MAX_LAYERS)
+  private val itemOddAngles: ArrayList<Int> = ArrayList(MAX_ITEMS_PER_LEVEL)
+  private val itemEvenAngles: ArrayList<Int> = ArrayList(MAX_ITEMS_PER_LEVEL)
+
   var baseView: View? = null
+  var constraintLayout: ConstraintLayout? = null
   var centerView: ImageView? = null
+
+  var middleItem: RadialPagerItem<T>? = null
+  val items: ArrayList<RadialPagerItem<T>> = ArrayList()
 
   constructor(context: Context?) : super(context) {
     initView()
@@ -23,7 +36,32 @@ class RadialPager<T> : ConstraintLayout {
 
   fun initView() {
     baseView = LayoutInflater.from(context).inflate(R.layout.radial_pager, this, true)
+    constraintLayout = baseView!!.findViewById(R.id.constraint_layout)
     centerView = baseView?.findViewById(R.id.center_item)
+
+    itemSizes.add(resources.getDimension(R.dimen.radialpager_center_item_size))
+    itemSizes.add(resources.getDimension(R.dimen.radialpager_first_level_item_size))
+    itemSizes.add(resources.getDimension(R.dimen.radialpager_second_level_item_size))
+    itemSizes.add(resources.getDimension(R.dimen.radialpager_third_level_item_size))
+
+    itemRadius.add(resources.getDimension(R.dimen.radialpager_center_level_radius))
+    itemRadius.add(resources.getDimension(R.dimen.radialpager_first_level_radius))
+    itemRadius.add(resources.getDimension(R.dimen.radialpager_second_level_radius))
+    itemRadius.add(resources.getDimension(R.dimen.radialpager_third_level_radius))
+
+    itemOddAngles.add(resources.getInteger(R.integer.radialpager_odd_level_first_item_angle))
+    itemOddAngles.add(resources.getInteger(R.integer.radialpager_odd_level_second_item_angle))
+    itemOddAngles.add(resources.getInteger(R.integer.radialpager_odd_level_third_item_angle))
+    itemOddAngles.add(resources.getInteger(R.integer.radialpager_odd_level_fourth_item_angle))
+    itemOddAngles.add(resources.getInteger(R.integer.radialpager_odd_level_fifth_item_angle))
+    itemOddAngles.add(resources.getInteger(R.integer.radialpager_odd_level_sixth_item_angle))
+
+    itemEvenAngles.add(resources.getInteger(R.integer.radialpager_even_level_first_item_angle))
+    itemEvenAngles.add(resources.getInteger(R.integer.radialpager_even_level_second_item_angle))
+    itemEvenAngles.add(resources.getInteger(R.integer.radialpager_even_level_third_item_angle))
+    itemEvenAngles.add(resources.getInteger(R.integer.radialpager_even_level_fourth_item_angle))
+    itemEvenAngles.add(resources.getInteger(R.integer.radialpager_even_level_fifth_item_angle))
+    itemEvenAngles.add(resources.getInteger(R.integer.radialpager_even_level_sixth_item_angle))
   }
 
   fun setText(text: String) {
@@ -34,5 +72,40 @@ class RadialPager<T> : ConstraintLayout {
 
   fun setCenterItem(centerItem: RadialPagerItem<T>) {
     centerView?.setImageResource(centerItem.imageResource!!)
+  }
+
+  fun setItems(items: ArrayList<RadialPagerItem<T>>) {
+    this.items.addAll(items)
+    renderInitialItems(this.items)
+  }
+
+  fun renderInitialItems(item: ArrayList<RadialPagerItem<T>>) {
+    var currentLayer: Int = 0
+    var currentItemPerLayer: Int = 0
+    item.forEachIndexed { index, radialPagerItem ->
+      renderItem(radialPagerItem, currentLayer, currentItemPerLayer)
+
+      currentItemPerLayer++
+      if (currentItemPerLayer >= MAX_ITEMS_PER_LEVEL) {
+        currentLayer++
+        currentItemPerLayer = 0
+      }
+    }
+  }
+
+  fun renderItem(radialPagerItem: RadialPagerItem<T>, layer: Int, itemPerLayer: Int) {
+    val item = CircleImageView(context)
+    item.setImageResource(radialPagerItem.imageResource!!)
+    val itemSize: Float = itemSizes.get(layer)
+    val layoutParams: ConstraintLayout.LayoutParams = ConstraintLayout.LayoutParams(itemSize.toInt(), itemSize.toInt())
+    layoutParams.circleConstraint = centerView!!.id
+    layoutParams.circleRadius = itemRadius.get(layer).toInt()
+    layoutParams.circleAngle = if (layer.isEven()) itemEvenAngles.get(itemPerLayer).toFloat() else itemOddAngles.get(itemPerLayer).toFloat()
+    item.layoutParams = layoutParams
+    constraintLayout!!.addView(item)
+  }
+
+  fun Int.isEven(): Boolean {
+    return this % 2 == 0
   }
 }
