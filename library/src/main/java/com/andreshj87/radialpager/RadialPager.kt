affad817aaad27d7af95183513cb65c18dd12d33
Andrés Hernández <andreshj87@gmail.com>
2018-04-13 23:56:19 +0200

    Add item movements

--- library/src/main/java/com/andreshj87/radialpager/RadialPager.kt
+++ library/src/main/java/com/andreshj87/radialpager/RadialPager.kt
@@ -23,12 +23,14 @@ class RadialPager<T> : ConstraintLayout, View.OnTouchListener {
   var baseView: View? = null
   var constraintLayout: ConstraintLayout? = null
   var centerView: ImageView? = null
+  var itemViews = ArrayList<View>()
 
   var middleItem: RadialPagerItem<T>? = null
   val items: ArrayList<RadialPagerItem<T>> = ArrayList()
 
-  var vertialCoordinate = 0
+  var verticalCoordinate = 0
   var previousVerticalCoordinate: Float = -1f
+  var blockMovement = false
 
   constructor(context: Context?) : super(context) {
     initView()
@@ -48,11 +50,13 @@ class RadialPager<T> : ConstraintLayout, View.OnTouchListener {
     itemSizes.add(resources.getDimension(R.dimen.radialpager_first_level_item_size))
     itemSizes.add(resources.getDimension(R.dimen.radialpager_second_level_item_size))
     itemSizes.add(resources.getDimension(R.dimen.radialpager_third_level_item_size))
+    itemSizes.add(resources.getDimension(R.dimen.radialpager_fourth_level_item_size))
 
     itemRadius.add(resources.getDimension(R.dimen.radialpager_center_level_radius))
     itemRadius.add(resources.getDimension(R.dimen.radialpager_first_level_radius))
     itemRadius.add(resources.getDimension(R.dimen.radialpager_second_level_radius))
     itemRadius.add(resources.getDimension(R.dimen.radialpager_third_level_radius))
+    itemRadius.add(resources.getDimension(R.dimen.radialpager_fourth_level_radius))
 
     itemOddAngles.add(resources.getInteger(R.integer.radialpager_odd_level_first_item_angle))
     itemOddAngles.add(resources.getInteger(R.integer.radialpager_odd_level_second_item_angle))
@@ -70,29 +74,125 @@ class RadialPager<T> : ConstraintLayout, View.OnTouchListener {
   }
 
   override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-    Log.i(javaClass.simpleName, event?.toString())
+    Log.i(javaClass.simpleName, "MotionEvent.y = " + event?.y)
 
     if (event?.action == MotionEvent.ACTION_UP) {
       snapViews()
-      vertialCoordinate = 0
+      verticalCoordinate = 0
       previousVerticalCoordinate = -1f
+      blockMovement = false
     } else if (event?.action == MotionEvent.ACTION_DOWN) {
-      vertialCoordinate = 0
+      verticalCoordinate = 0
       previousVerticalCoordinate = event.y
+      blockMovement = false
     } else if (event?.action == MotionEvent.ACTION_MOVE) {
       if (previousVerticalCoordinate < 0) {
         previousVerticalCoordinate = event.y
       } else {
         if (event.y > previousVerticalCoordinate) {
-          vertialCoordinate++
+          verticalCoordinate++
+          if (verticalCoordinate in 0..100 && !blockMovement) {
+            moveDownItems(verticalCoordinate / 4)
+          }
         } else {
-          vertialCoordinate--
+          //verticalCoordinate--
+          //moveUpItems(verticalCoordinate / 8)
         }
+        previousVerticalCoordinate = event.y
       }
-      Log.i(javaClass.simpleName, "Moving! " + vertialCoordinate)
+      var wtf = 2
+      wtf = 4
+      Log.i(javaClass.simpleName, "Moving! " + verticalCoordinate)
     }
 
     return true
+  }
+
+  private fun moveDownItems(percentage: Int) {
+    var layer: Int = 1
+    var itemPerLayer: Int = 0
+    itemViews.forEach {
+      val layoutParams: ConstraintLayout.LayoutParams = it.layoutParams as LayoutParams
+
+      if (itemPerLayer >= MAX_ITEMS_PER_LAYER) {
+        layer++
+        itemPerLayer = 0
+      }
+
+      val totalSize = itemSizes.get(layer - 1) - itemSizes.get(layer)
+      if (layer == 1) {
+        Log.i(javaClass.simpleName, "totalSize between layer 1 and 0 = " + totalSize)
+      }
+      val totalRadius = itemRadius.get(layer) - itemRadius.get(layer - 1)
+      //val totalAngle = if (layer.isEven()) itemEvenAngles.get(itemPerLayer) - itemEvenAngles.get( (itemPerLayer - 1).rightMod(6) )
+//        else itemOddAngles.get(itemPerLayer) - itemOddAngles.get( (itemPerLayer - 1).rightMod(6) )
+
+      val dSize = (percentage * totalSize) / 100
+      if (layer == 1) {
+        Log.i(javaClass.simpleName, "dSize = " + dSize)
+      }
+      val dRadius = (percentage * totalRadius) / 100
+  //    val dAngle = (percentage * totalAngle) / 100
+
+      if (layer==1) {
+        Log.i(javaClass.simpleName, "Layer[0].size = " + itemSizes.get(layer-1))
+        Log.i(javaClass.simpleName, "CurrentLayerSize = " + layoutParams.width)
+      }
+
+      val currentSize = layoutParams.width + dSize
+      if (layer == 1) {
+        Log.i(javaClass.simpleName, "ELSE, currentSize = " + currentSize)
+      }
+      val currentRadius = layoutParams.circleRadius - dRadius
+//        val currentAngle = layoutParams.circleAngle - dAngle
+
+      layoutParams.width = currentSize.toInt()
+      layoutParams.height = currentSize.toInt()
+      layoutParams.circleRadius = currentRadius.toInt()
+      //layoutParams.circleAngle = currentAngle
+      it.layoutParams = layoutParams
+
+      if (layoutParams.width > itemSizes.get(layer-1)) {
+        Log.i(javaClass.simpleName, "Estoy en el if que lo para todo!!")
+        blockMovement = true
+      }
+
+      itemPerLayer++
+    }
+  }
+
+  fun moveUpItems(percentage: Int) {
+    var layer: Int = 1
+    var itemPerLayer: Int = 0
+    itemViews.forEach {
+      val layoutParams: ConstraintLayout.LayoutParams = it.layoutParams as LayoutParams
+
+      if (itemPerLayer >= MAX_ITEMS_PER_LAYER) {
+        layer++
+        itemPerLayer = 0
+      }
+
+      val dSize = itemSizes.get(layer) - itemSizes.get(layer + 1)
+      val dRadius = itemRadius.get(layer) - itemRadius.get(layer + 1)
+      val dAngle = if (layer.isEven()) itemEvenAngles.get(itemPerLayer) - itemEvenAngles.get(  (itemPerLayer + 1).rightMod(6) )
+      else itemOddAngles.get(itemPerLayer) - itemOddAngles.get( (itemPerLayer + 1).rightMod(6) )
+
+      val currentSize = layoutParams.width - ((percentage * dSize) / 100)
+      val currentRadius = layoutParams.circleRadius + ((percentage * dRadius) / 100)
+      val currentAngle = layoutParams.circleAngle + ((percentage * dAngle) / 100)
+
+      if (currentSize < itemSizes.get(layer + 1)) {
+        return
+      } else {
+        layoutParams.width = currentSize.toInt()
+        layoutParams.height = currentSize.toInt()
+        layoutParams.circleRadius = currentRadius.toInt()
+        layoutParams.circleAngle = currentAngle
+        it.layoutParams = layoutParams
+
+        itemPerLayer++
+      }
+    }
   }
 
   fun setText(text: String) {
@@ -141,6 +241,7 @@ class RadialPager<T> : ConstraintLayout, View.OnTouchListener {
     layoutParams.circleRadius = itemRadius.get(layer).toInt()
     layoutParams.circleAngle = if (layer.isEven()) itemEvenAngles.get(itemPerLayer).toFloat() else itemOddAngles.get(itemPerLayer).toFloat()
     item.layoutParams = layoutParams
+    itemViews.add(item)
     constraintLayout!!.addView(item)
   }
 
@@ -157,6 +258,7 @@ class RadialPager<T> : ConstraintLayout, View.OnTouchListener {
     layoutParams.circleRadius = itemRadius.get(layer).toInt()
     layoutParams.circleAngle = if (layer.isEven()) itemEvenAngles.get(itemPerLayer).toFloat() else itemOddAngles.get(itemPerLayer).toFloat()
     item.layoutParams = layoutParams
+    itemViews.add(item)
     constraintLayout!!.addView(item)
   }
 
@@ -166,5 +268,12 @@ class RadialPager<T> : ConstraintLayout, View.OnTouchListener {
 
   fun Int.isEven(): Boolean {
     return this % 2 == 0
+  }
+
+  fun Int.rightMod(mod: Int): Int {
+    var r = this.rem(mod)
+    if (r < 0) r += mod
+
+    return r
   }
 }
